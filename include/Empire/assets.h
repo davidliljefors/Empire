5e2a36911b1cee6fe05e8d5e7e3fcb25402994ef
David Liljefors <david@ortolan.se>
2026-01-26 08:16:18 +0100

    define asset loader interface

--- include/Empire/assets.h
+++ include/Empire/assets.h
@@ -7,12 +7,23 @@ typedef struct emp_asset_t
     emp_buffer data;
     u64 size;
     u64 hash;
+    void* handle;
 } emp_asset_t;
+
+typedef struct emp_asset_loader_t
+{
+    u64 target_type;
+    void* (*load)(emp_buffer buffer);
+    void (*unload)(void* handle);
+} emp_asset_loader_t;
 
 typedef struct emp_asset_manager_o
 {
     emp_asset_t* assets;
     u64 asset_count;
+    emp_asset_loader_t* loaders;
+    u64 loader_count;
 } emp_asset_manager_o;
+
 
 void emp_asset_manager_check_hot_reload();
