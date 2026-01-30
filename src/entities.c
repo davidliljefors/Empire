57b746dde3d77b3f1693ca680df520d8b6ce9898
David Liljefors <david@ortolan.se>
2026-01-30 16:59:48 +0100

    add entitites

---
+++ src/entities.c
@@ -0,0 +1,21 @@
+#include "entities.h"
+
+typedef struct emp_player_t
+{
+
+};
+
+typedef struct emp_enemy_t
+{
+
+};
+
+typedef struct emp_bullet_t
+{
+
+};
+
+typedef struct emp_bullet_generator_t
+{
+
+};
