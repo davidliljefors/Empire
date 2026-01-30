a8499d7ff1c6e6f56a575555cd18841a90fbca8a
David Liljefors <david@ortolan.se>
2026-01-30 17:50:17 +0100

    add shit

--- src/entities.c
+++ src/entities.c
@@ -1,21 +1,6 @@
 #include "entities.h"
 
-typedef struct emp_player_t
+void emp_entities_update(float dt)
 {
 
-};
-
-typedef struct emp_enemy_t
-{
-
-};
-
-typedef struct emp_bullet_t
-{
-
-};
-
-typedef struct emp_bullet_generator_t
-{
-
-};
+}
---
+++ src/entities.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <Empire/types.h>
+
+#define EMP_MAX_PLAYERS 1;
+typedef struct emp_player_t
+{
+
+} emp_player_t;
+
+#define EMP_MAX_ENEMIES 256;
+typedef struct emp_enemy_t
+{
+
+} emp_enemy_t;
+
+#define EMP_MAX_BULLETS 65535;
+typedef struct emp_bullet_t
+{
+
+} emp_bullet_t;
+
+#define EMP_MAX_BULLET_GENERATORS 1024;
+typedef struct emp_bullet_generator_t
+{
+
+} emp_bullet_generator_t;
+
+typedef struct emp_entities_t
+{
+	emp_player_t* player;
+	emp_enemy_t* enemies;
+	emp_bullet_t* bullets;
+	emp_bullet_generator_t* generators;
+} emp_entities_t;
+
+extern emp_entities_t* G;
+
+void emp_entities_update(float dt, emp_entities_t*, u32 count);
